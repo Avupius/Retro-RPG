57d7ea1f54a0fa3c7beb8eeb65a6be36a1c14b1e
Cezary Kutko <kutko.cezary@fh-swf.de>
2025-09-19 13:45:14 +0200

    Angriff mit Sprites

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame 
 import json
+from pygame.transform import scale
 import pygame_menu
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos):
+    def __init__(self, pos, attack_sprites = None, enemies_group = None):
         super().__init__()
         self.spritesheet = pygame.image.load("assets/Cute_Fantasy_Free/Player/Player.png").convert_alpha()
 
@@ -30,6 +31,25 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.Vector2(self.rect.topleft)
         self.speed = 100
 
+        #Angrifswerte
+        self.attack_damage = 25
+        self.attack_range = 28
+        self.attack_width = 14
+        self.attack_duration = 120
+        self.attack_cooldown = 350
+        self.last_attack_time = -1_000_000
+
+        self.mode = "idle"
+        self.attack_timer = 0.0
+        self.attack_frame_index = 0
+        self.attack_dir_on_start = "down"
+        self.hitbox_spawned_segments = set()
+        self.attack_frames_seq = self.load_attack_sequence(rows=(6, 7, 8), cols=4)
+        self.attack_anim_speed = 0.07
+
+        self.attack_sprites = attack_sprites
+        self.enemies_group = enemies_group
+
     def load_frames(self, row):
         frames = []
         for i in range(self.frames_per_row):
@@ -41,62 +61,172 @@ class Player(pygame.sprite.Sprite):
             frames.append(frame)
         return frames
 
+    def load_attack_sequence(self, rows = (6, 7, 8), cols = 4):
+        seq = []
+        for row in rows:
+            for i in range(cols):
+                frame = pygame.Surface((self.frame_width, self.frame_height), pygame.SRCALPHA)
+                x = i * self.frame_width
+                y = row * self.frame_height
+                frame.blit(self.spritesheet, (0, 0), pygame.Rect(x, y, self.frame_width, self.frame_height))
+                frame = pygame.transform.scale(frame, (self.frame_width * self.frames_scale, self.frame_height * self.frames_scale))
+                seq.append(frame)
+        return seq
+
 
     def update(self, dt, collision_rect):
         keys = pygame.key.get_pressed()
-        dx, dy = 0, 0
-        moving = False
-        
-        if keys[pygame.K_w]:
-            dy -= self.speed * dt
-            self.direction = "up"
-            moving = True
-        if keys[pygame.K_s]:
-            dy += self.speed * dt
-            self.direction = "down"
-            moving = True
-        if keys[pygame.K_a]:
-            dx -= self.speed * dt
-            self.direction = "left"
-            moving = True
-        if keys[pygame.K_d]:
-            dx += self.speed * dt
-            self.direction = "right"
-            moving = True
-        
-        #Bewegung in X/Y-Richtung und Kollision prüfung
-        self.pos.x += dx
-        self.rect.x = round(self.pos.x)
-        for rect in collision_rect:
-            if self.rect.colliderect(rect):
-                self.pos.x -= dx
-                self.rect.x = round(self.pos.x)
-        
+        if self.mode != "attack":
+            dx, dy = 0, 0
+            moving = False
+            
+            if keys[pygame.K_w]:
+                dy -= self.speed * dt
+                self.direction = "up"
+                moving = True
+            if keys[pygame.K_s]:
+                dy += self.speed * dt
+                self.direction = "down"
+                moving = True
+            if keys[pygame.K_a]:
+                dx -= self.speed * dt
+                self.direction = "left"
+                moving = True
+            if keys[pygame.K_d]:
+                dx += self.speed * dt
+                self.direction = "right"
+                moving = True
+            
+            #Bewegung in X/Y-Richtung und Kollision prüfung
+            self.pos.x += dx
+            self.rect.x = round(self.pos.x)
+            for rect in collision_rect:
+                if self.rect.colliderect(rect):
+                    self.pos.x -= dx
+                    self.rect.x = round(self.pos.x)
+            
 
-        self.pos.y += dy
-        self.rect.y = round(self.pos.y)
-        for rect in collision_rect:
-            if self.rect.colliderect(rect):
-                self.pos.y -= dy
-                self.rect.y = round(self.pos.y)
-        
+            self.pos.y += dy
+            self.rect.y = round(self.pos.y)
+            for rect in collision_rect:
+                if self.rect.colliderect(rect):
+                    self.pos.y -= dy
+                    self.rect.y = round(self.pos.y)
+            
 
-        #Animationen
-        if moving:
-            self.animation_timer += dt
-            if self.animation_timer >= 0.1:
-                self.animation_timer = 0
-                self.frame_index = (self.frame_index + 1) % self.frames_per_row
+            #Animationen
+            if moving:
+                self.animation_timer += dt
+                if self.animation_timer >= 0.1:
+                    self.animation_timer = 0
+                    self.frame_index = (self.frame_index + 1) % self.frames_per_row
                 self.image = self.frames[self.direction][self.frame_index]
-                
+                self.mode = "move"
+                    
+            else:
+                #Stillstand erstes Frame
+                self.frame_index = 0
+                self.image = self.frames[self.direction][self.frame_index]
+                self.mode = "move"
+            
+            if keys[pygame.K_SPACE]:
+                self.attack()
         else:
-            #Stillstand erstes Frame
-            self.frame_index = 0
-            self.image = self.frames[self.direction][self.frame_index]
+            self.attack_timer += dt
+            if self.attack_timer >= self.attack_anim_speed:
+                self.attack_timer -= self.attack_anim_speed
+                self.attack_frame_index += 1
+                if self.attack_frame_index >= 3:
+                    self.mode = "idle"
+                    self.attack_frame_index = 0
+
+
+            seg_map = {"right": 0, "down": 3, "left": 6, "up": 9}
+            base = seg_map[self.attack_dir_on_start]
+            frame_idx = base + self.attack_frame_index
+
+            self.image = self.attack_frames_seq[frame_idx]
+
+            if self.attack_frame_index == 1:
+                self.spawn_attack_hitbox(direction_override=self.attack_dir_on_start)
+                self.attack_frame_index = 2
+
 
     def draw_player(self, surface, scale):
         scaled_image = pygame.transform.scale(self.image, (self.image.get_width() * scale, self.image.get_height() * scale))
         surface.blit(scaled_image, (round(self.rect.x * scale), round(self.rect.y * scale)))
+
+    def attack(self):
+        if self.attack_sprites is None or self.enemies_group is None:
+            return
+        now = pygame.time.get_ticks()
+        if now - self.last_attack_time < self.attack_cooldown:
+            return
+        self.last_attack_time = now
+        self.mode = "attack"
+        self.attack_timer = 0.0
+        self.attack_frame_index = 0
+        self.attack_dir_on_start = self.direction
+        self.hitbox_spawned_segments.clear()
+    
+    def spawn_attack_hitbox(self, direction_override=None):
+        direction = direction_override or self.direction
+        hitbox = AttackHitbox(
+            owner = self,
+            damage = self.attack_damage,
+            range_px = self.attack_range,
+            width_px = self.attack_width,
+            duration_ms = self.attack_duration,
+            direction = self.direction,
+            enemies_group = self.enemies_group)
+
+        self.attack_sprites.add(hitbox)
+
+class AttackHitbox(pygame.sprite.Sprite):
+    def __init__(self, owner, damage, range_px, width_px, duration_ms, direction, enemies_group):
+        super().__init__()
+        self.owner = owner
+        self.damage = damage
+        self.duration_ms = duration_ms
+        self.enemies_group = enemies_group
+        self.spawn_time = pygame.time.get_ticks()
+        self.hit_enemies = set()
+
+
+        px, py, pw, ph = owner.rect
+        if direction == "right":
+            rect = pygame.Rect(px + pw, py + (ph - width_px)//2, range_px, width_px)
+        elif direction == "left":
+            rect = pygame.Rect(px - range_px, py + (ph - width_px)//2, range_px, width_px)
+        elif direction == "up":
+            rect = pygame.Rect(px + (pw - width_px)//2, py - range_px,width_px, range_px)
+        elif direction == "down":
+            rect = pygame.Rect(px + (pw - width_px)//2, py + ph, width_px, range_px)
+
+        self.image = pygame.Surface((rect.width, rect.height), pygame.SRCALPHA)
+        self.image.fill((255, 255, 0, 140))
+        self.rect = self.image.get_rect(topleft = rect.topleft)
+
+    def update(self, *_):
+        if pygame.time.get_ticks() - self.spawn_time > self.duration_ms:
+            self.kill()
+            return
+
+        for enemy in pygame.sprite.spritecollide(self, self.enemies_group, False):
+            if enemy not in self.hit_enemies:
+                if hasattr(enemy, "take damage"):
+                    enemy.take_damage(self.damage)
+                self.hit_enemies.add(enemy)
+
+    def draw(self, surface, scale: int = 1):
+        if scale == 1:
+            surface.blit(self.image, self.rect.topleft)
+        else:
+            scaled_img = pygame.transform.scale(
+                self.image,
+                (int(self.rect.width * scale), int(self.rect.height * scale))
+            )
+            surface.blit(scaled_img, (round(self.rect.x * scale), round(self.rect.y * scale)))
 
 
 class LoadMap:
@@ -212,6 +342,8 @@ class LoadMap:
         return areas
 
 
+
+
 # -- Game-Loop Funktionen --
 def check_interactions(player, current_map):
     for area, props in current_map.interact_areas:
@@ -248,11 +380,18 @@ menu.add.button("Spiel beenden", pygame_menu.events.EXIT)
 menu.mainloop(screen)
 
 
+attack_sprites =pygame.sprite.Group()
+
+enemies = pygame.sprite.Group()
+
 # -- Player erstellen --
-player = Player((100,80))
+player = Player((100,80), attack_sprites = attack_sprites, enemies_group = enemies)
 
 # -- Game Loop--
 while running:
+
+    # Frames Berechnung
+    dt = clock.tick(60) / 1000
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -264,15 +403,18 @@ while running:
     collision_rects = game_map.get_collision_rects()
     player.update(dt, collision_rects)
     
+    attack_sprites.update()
+
     game_map = check_interactions(player, game_map)
 
     # Bildshirm zeichen
     game_map.draw(screen)
     player.draw_player(screen,scale=screen_scale)
-    pygame.display.flip()
+    
+    for hb in attack_sprites:
+        hb.draw(screen, scale=screen_scale)
 
-    # Frames Berechnung
-    dt = clock.tick(60) / 1000
+    pygame.display.flip()
 
 
 pygame.quit()
